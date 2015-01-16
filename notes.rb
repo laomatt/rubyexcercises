bd7d475e4c2d01fbc9987638abd1c7c1071e3d95
laomatt <laomatt1@gmail.com>
2015-01-15 23:23:51 -0800

    top three

--- notes.rb
+++ notes.rb
@@ -104,3 +104,116 @@ Hash[ [ ["a", 100], ["b", 200] ] ]   #=> {"a"=>100, "b"=>200}
 Hash["a" => 100, "b" => 200]         #=> {"a"=>100, "b"=>200}
 
 ################################################################################################################################
+
+
+def group_and_count(input)
+  ##modified if-else statement
+  return nil if input.nil? || input.empty?
+  input.each_with_object(Hash.new(0)){ |k, h| h[k] += 1 }
+end
+
+####################################################################################################  SHERLOCK  ############################################################################################################################################################
+
+
+Sherlock has to find suspect on his latest case. He will use your method, dear Watson. The suspect in this case is a number which is most unique in given set, e.g.:
+
+has least (minimum) occurences in set
+and no other number has the same occurences count
+Write method which helps Sherlock to find suspect from given set of numbers. If no suspect is found the method should return nil.
+
+find_suspect(1, 2, 3, 4, 2, 2, 1, 4, 4) # => 3
+find_suspect(1, 1, 2, 2) # => nil
+find_suspect(1, 1, 2, 2, 2) # => 1
+find_suspect(1, 2, 3, 4, 4, 3) # => nil
+
+{'x'=>65,'b'=>88,'r'=>9,'t'=>677,'a'=>12,'v'=>85,'c'=>55}
+  ## solution ##
+
+  def find_suspect(*numbers)
+    #first found an array of frequencies of each entry
+freq=numbers.map { |e| numbers.find_all {|f| f==e}.length  }
+
+#then hashed it over to its entries, so I have a hash of entries and freqencies
+freq2=Hash[numbers.zip(freq)]
+freq2=Hash[numbers.zip(numbers.map { |e| numbers.find_all {|f| f==e}.length  })]
+
+#then I took another derivative, found the frequencies of these freqencies,
+freq2.values.map { |e| freq2.values.find_all {|f| f==e}.length }
+
+#and hashed it over to the orig hash
+Hash[freq2.to_a.zip(freq2.values.map { |e| freq2.values.find_all {|f| f==e}.length })]
+
+#then I found all the entries in the double hash for which v (the frequency of the frequency equals one)
+freq3=Hash[freq2.to_a.zip(freq2.values.map { |e| freq2.values.find_all {|f| f==e}.length })].find_all{|k,v| v==1}
+
+#now i take the values for which the frequency of the frequency is 1 (freq3) and single out the min by keys, and take its firt entry (the key)
+
+f4=Hash[Hash[freq3].keys].min_by {|k,v| v}.to_a.first
+#freq2[f4]
+#freq2.min_by {|k,v| v}.to_a.first
+(f4 != freq2.min_by {|k,v| v}.to_a.first)? nil : f4
+
+  end
+
+###### MY TWO LINE SOLUTION #########
+f4=Hash[Hash[Hash[Hash[numbers.zip(numbers.map { |e| numbers.find_all {|f| f==e}.length  })].to_a.zip(Hash[numbers.zip(numbers.map { |e| numbers.find_all {|f| f==e}.length  })].values.map { |e| Hash[numbers.zip(numbers.map { |e| numbers.find_all {|f| f==e}.length  })].values.find_all {|f| f==e}.length })].find_all{|k,v| v==1}].keys].min_by {|k,v| v}.to_a.first
+(f4 != Hash[numbers.zip(numbers.map { |e| numbers.find_all {|f| f==e}.length  })].min_by {|k,v| v}.to_a.first)? nil : f4
+
+########################  BETTER SOLUTIONS ######################
+# return suspect number from given set, return nil if no suspect found
+def find_suspect(*numbers)
+  hash = numbers.each_with_object(Hash.new 0) { |n, o| o[n] += 1 }
+  hash.select! { |k, v| v == hash.values.min }
+  hash.size == 1 ? hash.keys.first : nil
+end
+
+
+
+# return suspect number from given set, return nil if no suspect found
+def find_suspect(*numbers)
+  min = numbers.min_by{|i| numbers.count i}
+  min if min == numbers.reverse.min_by{|i| numbers.count i}
+end
+
+
+
+
+# return suspect number from given set, return nil if no suspect found
+def find_suspect(*numbers)
+  ((x, y), (z, _)) = numbers.group_by(&:to_i).map{|x, y| [y.length, x]}.sort_by{|(x, _)| x}
+  x != z ? y : nil
+end
+################################################################################################################################################################################################################################################################
+
+Email addresses are notoriously difficult to validate.
+
+Create a method that takes an email and returns true if valid, false otherwise.
+
+Tip: It doesn't have to be perfect.
+
+For example:
+
+validate('joe@example.com') => true
+validate('joe') => false
+Here's tool that may help: http://rubular.com/
+
+I suggest not using some code that you find on the internet. Yes, there are regexes that do email validation. But what is the purpose of copying and pasting?
+
+Also, you do not need to use a regex. There are many ways of solving this.
+
+  #my solution
+
+  #solution using regex
+def validate(email)
+  /^.+@.+\..+$/ === email
+end
+
+def validate(email)
+  !!(email =~ /^[\w\.\-]+@[\w\-]+(\.\w+)+$/)
+end
+
+def validate(email)
+  !!(email =~ /.+@.+\..+/)
+end
+
+
